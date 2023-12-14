67f6d0a5059b17c06ad4b85b77e98c105741b6ea
깃hanseon97@naver.com <hanseon97@naver.com>
2023-12-14 17:49:54 +0900

    restart

--- src/main/java/org/example/Article.java
+++ src/main/java/org/example/Article.java
@@ -1,29 +1,15 @@
-package org.example;
-
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
+@AllArgsConstructor
 public class Article {
-
     private int id;
-    private String content;
     private String title;
-
-    public Article(int id, String content, String title){
-            this.id = id;
-            this.content = content;
-            this.title = title;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+    private String content;
 }
+
+
+
